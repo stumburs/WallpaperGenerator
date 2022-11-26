e57bb92f6abccf62626fa670146e0bc6fe12b4db
stumburs <stumburs@inbox.lv>
2022-11-26 12:53:50 +0200

    Added switches for active program state.

--- WallpaperGenerator/WallpaperGenerator.cpp
+++ WallpaperGenerator/WallpaperGenerator.cpp
@@ -13,6 +13,17 @@ const int kWindowHeight = 900;
 
 bool shader_on = false;
 
+enum Algorithm
+{
+	NONE = 0,
+	FLOWFIELD,
+	MODE2
+};
+
+int active_algorithm = Algorithm::NONE;
+
+char text[10] = {};
+
 int main()
 {
 	// Init window
@@ -27,6 +38,9 @@ int main()
 	// Create flowfield preset thing
 	Flowfield f;
 
+	// Default - BLEND_ADD_COLORS
+	int active_blend_mode = 3;
+
 	GuiSetStyle(DEFAULT, TEXT_SIZE, 20);
 	GuiLoadStyle("/include/style/gui/dark.rgs");
 
@@ -34,9 +48,24 @@ int main()
 	{
 		// Update
 		{
-
-			// Update flowfield
-			f.Update();
+			switch (active_algorithm)
+			{
+			case NONE:
+				if (text[0] == '1')
+				{
+					active_algorithm = Algorithm::FLOWFIELD;
+				}
+				break;
+			case FLOWFIELD:
+				// Update flowfield
+				f.Update();
+				break;
+			case MODE2:
+				break;
+			default:
+				break;
+			}
+			
 		}
 
 		// Draw
@@ -44,22 +73,65 @@ int main()
 		{
 			ClearBackground(RAYWHITE);
 
-			// Preview Box
-			DrawRectangle(kWindowWidth - 850, 50, 800, 450, BLACK);
-			DrawText("Preview", kWindowWidth - 425 - MeasureText("PREVIEW", 40) / 2, 250, 40, {255, 255, 255, 60});
+			switch (active_algorithm)
+			{
+			case NONE:
+				GuiTextBox({ 40, 40, 100, 40 }, text, 10, 1);
+				break;
+			case FLOWFIELD:
+				// Preview Box
+				DrawRectangle(kWindowWidth - 850, 50, 800, 450, BLACK);
 
-			shader_on = GuiCheckBox({ kWindowWidth - 850, 525, 40, 40 }, "SHADER", shader_on);
+				shader_on = GuiCheckBox({ kWindowWidth - 850, 525, 40, 40 }, "SHADER", shader_on);
 
-			BeginBlendMode(BLEND_ADD_COLORS);
+				active_blend_mode = GuiScrollBar({ 60, 60, 400, 40 }, active_blend_mode, 0, 6);
 
-			// Apply shader
-			if (shader_on)
-				BeginShaderMode(shader);
+				switch (active_blend_mode)
+				{
+				case 0:
+					DrawText("BLEND_ALPHA", 180, 100, 20, BLACK);
+					break;
+				case 1:
+					DrawText("BLEND_ADDITIVE", 180, 100, 20, BLACK);
+					break;
+				case 2:
+					DrawText("BLEND_MULTIPLIED", 180, 100, 20, BLACK);
+					break;
+				case 3:
+					DrawText("BLEND_ADD_COLORS", 180, 100, 20, BLACK);
+					break;
+				case 4:
+					DrawText("BLEND_SUBTRACT_COLORS", 180, 100, 20, BLACK);
+					break;
+				case 5:
+					DrawText("BLEND_ALPHA_PREMULTIPLY", 180, 100, 20, BLACK);
+					break;
+				case 6:
+					DrawText("BLEND_CUSTOM", 180, 100, 20, BLACK);
+					break;
+				default:
+					break;
+				}
 
-			DrawTexturePro(f.image.texture, { 0, 0, (float)f.image.texture.width, (float)-f.image.texture.height }, { kWindowWidth - 850, 50, 800, 450}, {0, 0}, 0.0f, WHITE);
-			EndShaderMode();
+				BeginBlendMode(active_blend_mode);
 
-			EndBlendMode();
+				// Apply shader
+				if (shader_on)
+					BeginShaderMode(shader);
+
+				DrawTexturePro(f.image.texture, { 0, 0, (float)f.image.texture.width, (float)-f.image.texture.height }, { kWindowWidth - 850, 50, 800, 450 }, { 0, 0 }, 0.0f, WHITE);
+				EndShaderMode();
+
+				EndBlendMode();
+				DrawText("Preview", kWindowWidth - 425 - MeasureText("PREVIEW", 40) / 2, 250, 40, { 255, 255, 255, 60 });
+				
+				break;
+			case MODE2:
+				break;
+			default:
+				break;
+			}
+
 			DrawFPS(20, 20);
 
 			if (shader_on)
