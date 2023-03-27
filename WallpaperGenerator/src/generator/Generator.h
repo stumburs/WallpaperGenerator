782e3cae5e2406eb62ef7f29e0f8a9990ddf7862
stumburs <stumburs@inbox.lv>
2023-03-28 01:59:05 +0300

    Tidied up generators.

--- WallpaperGenerator/src/generator/Generator.h
+++ WallpaperGenerator/src/generator/Generator.h
@@ -30,7 +30,6 @@ public:
 	std::vector<Setting> default_settings;
 	std::vector<Setting> user_settings;
 
-	//virtual void Init() = 0;
 	virtual void Update() = 0;
 	virtual void ApplySettings() = 0;
 	virtual void ResetSettings() = 0;
