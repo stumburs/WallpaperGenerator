e7b39852d4765cdb849ffe454c473388575e8d30
stumburs <stumburs@inbox.lv>
2023-05-25 15:34:54 +0300

    Cleaned up JSONReader comments

--- WallpaperGenerator/src/json/JSONReader.h
+++ WallpaperGenerator/src/json/JSONReader.h
@@ -23,15 +23,3 @@ namespace JSONReader
 	// Loads settings from json into custom setting data type by specifying json path
 	std::pair<std::vector<std::string>, std::unordered_map<std::string, Generator::Setting>> LoadSettingsFromJson(std::string path);
 }
-
-//std::ostream& operator<<(std::ostream& os, Setting& setting)
-//{
-//	return os << '\t' << setting.input_type << '\n' << '\t' << setting.tooltip << '\n' << '\t' << setting.value << '\n' << '\t' << setting.precision << '\n' << '\t' << setting.range.first << '\t' << setting.range.second << '\n' << '\t' << std::boolalpha << setting.text_box_editable << '\n';
-//}
-
-//void PrintSettings(std::unordered_map<std::string, Setting> settings)
-//{
-//	for (auto& s : settings)
-//		std::cout << s.first << '\n' << s.second << '\n';
-//	std::cout << '\n';
-//}
